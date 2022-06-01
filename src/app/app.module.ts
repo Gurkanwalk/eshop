d1461cce5d481c887370dd5928c322650d947b7d
Gurkanwalk <74870271+Gurkanwalk@users.noreply.github.com>
2022-06-01 19:32:23 +0530

    home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { routes } from './app.router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductsComponent } from './components/products/products.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductsComponent,
+    GalleryComponent,
+    ContactComponent,
+    SignupComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routes
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.router.ts
@@ -0,0 +1,21 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ProductsComponent } from './components/products/products.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { SignupComponent } from './components/signup/signup.component';
+
+import { AppComponent } from './app.component';
+
+export const router: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' }, //default page
+    { path: 'home', component: HomeComponent },
+    { path: 'products', component: ProductsComponent },
+    { path: 'gallery', component: GalleryComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: 'signup', component: SignupComponent },
+    
+];
+
+export const routes = RouterModule.forRoot(router);
